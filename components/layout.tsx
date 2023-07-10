0b4fbaffd2b900c4ba3f9b2a9f1651665e5e586d
s4birli <sabirli31@gmail.com>
2023-07-10 03:59:59 +0100

    initial

---
+++ components/layout.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+import Head from "next/head";
+
+import Header from "./header";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => (
+  <>
+    <Head>
+      <title>Next.js with Auth0</title>
+    </Head>
+
+    <Header />
+
+    <main>
+      <div className="container">{children}</div>
+    </main>
+
+    <style jsx>{`
+      .container {
+        max-width: 42rem;
+        margin: 1.5rem auto;
+      }
+    `}</style>
+    <style jsx global>{`
+      body {
+        margin: 0;
+        color: #333;
+        font-family: -apple-system, "Segoe UI";
+      }
+    `}</style>
+  </>
+);
+
+export default Layout;
